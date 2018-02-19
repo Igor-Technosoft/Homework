ad626ac9b7e13bb9a22158c9c490dce503400184
Igor Sandler <igor@geteverwise.com>
2018-02-18 20:18:21 -0500

    Test

--- SignUpPage.java
+++ SignUpPage.java
@@ -12,13 +12,21 @@ public class SignUpPage extends BasePage {
     private By phoneNumberTextField = By.xpath("//*[@id=\"u_0_r\"]");
     private By createAccountButton = By.xpath("//*[@id=\"u_0_14\"]");
     private By newPasswordTextField = By.xpath("//*[@id=\"u_0_y\"]");
-    private By genderRadioButton = By.xpath("//*[@id=\"u_0_d\"]");
+    private By genderRadioButton = By.id("u_0_9");
+  private By monthDropdownField = By.id("month");
+  private By dayDropDownField = By.id("day");
+  private By yearDropDownField = By.id("year");
+  private String monthValue = "Month";
+  private String dayValue = "Day";
+  private String yearValue = "Year";
 
-public void enterFirstName (String firstName) {
+
+    public void enterFirstName (String firstName) {
     setValueToInputField(firstName,firstNameTextField);
 }
 
 public void enterLastName (String lastName) {
+
     setValueToInputField(lastName,lastNameTextField);
 
 }
@@ -29,12 +37,53 @@ public void enterPhoneNumber (String phoneNumber) {
 public void enterNewPassword (String newPassword) {
     setValueToInputField(newPassword,newPasswordTextField);
 }
+// Click Radio Button
 public void selectGenderButton () {
     clickOn(genderRadioButton);
 }
+
+  //  public void chooseMonthDropDown()  {
+   //     selectMonthFromDropDown(monthDropdownField);
+
+   // }
+
+    // Method Overloading
+   public void chooseDifferentMonthDropDown() {
+        selectMonthFromDropDown(monthDropdownField,monthValue);
+   }
+
+
+
+    //public void chooseDayDropDown() {
+     //   selectDayFromDropDown(dayDropDownField);
+    //}
+
+    // Method Overloading
+    public void chooseDifferentDayDropDown() {
+        selectDayFromDropDown(dayDropDownField,dayValue);
+    }
+
+    //public void chooseYearDropDown() {
+     //   selectYearDropDown(yearDropDownField);
+    //}
+
+    //Method Overloading
+    public void chooseDifferentYearDropDown() {
+        selectYearDropDown(yearDropDownField,yearValue);
+    }
+
+
+
 public void clickCreateButton() {
+   // chooseMonthDropDown();
+   // chooseDayDropDown();
+   // chooseYearDropDown();
+    chooseDifferentMonthDropDown();
+    chooseDifferentDayDropDown();
+    chooseDifferentYearDropDown();
     clickOn(createAccountButton);
 }
 
 
 }
+
